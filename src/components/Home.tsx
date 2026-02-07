bf9f2f205253c949016e00bd61078337e1f7ba4a
BrayanTM <brayantebelan@gmail.com>
2026-02-06 23:17:47 -0600

    feat: ajusta estilos y mejora la responsividad en el componente Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,7 +15,7 @@ export const Home = () => {
     confetti({
       particleCount: 100,
       spread: 70,
-      origin: { y: 0.6 },
+      origin: { y: 0.8 },
     });
   };
 
@@ -23,41 +23,41 @@ export const Home = () => {
     <>
       <section
         id="home"
-        className="flex min-h-screen scroll-mt-16 flex-col items-center justify-center bg-slate-300 p-8 pt-24 dark:bg-gray-950"
+        className="flex min-h-screen scroll-mt-16 flex-col items-center justify-center bg-slate-300 p-8 pt-24 md:p-14 md:pt-24 lg:p-22 dark:bg-gray-950"
       >
         <div className="flex flex-col items-center justify-center text-center md:flex-row md:items-start md:justify-between md:text-left">
-          <div className="md:mr-16 md:w-1/2">
-            <div className="flex max-w-3xl flex-col items-center text-center md:items-start md:text-left">
-              <h1 className="mb-4 text-5xl font-bold text-gray-800 md:text-6xl dark:text-gray-200">
+          <div className="md:w-1/2">
+            <div className="flex flex-col items-center text-center md:items-start md:text-left lg:mt-12 lg:max-w-3xl xl:max-w-4xl">
+              <h1 className="mb-4 text-5xl font-bold text-gray-800 lg:text-6xl dark:text-gray-200">
                 {t.home.greeting} <p className="text-blue-600">{t.home.name}</p>
               </h1>
-              <h1 className="mb-6 text-2xl font-semibold text-gray-800 dark:text-gray-200">
+              <h1 className="mb-6 text-xl font-semibold text-gray-800 lg:text-2xl dark:text-gray-200">
                 {t.home.title}
               </h1>
-              <p className="mb-3 text-lg text-gray-600 dark:text-gray-400">
+              <p className="text-md mb-3 text-gray-600 lg:text-lg xl:text-xl dark:text-gray-400">
                 {t.home.description}
               </p>
             </div>
-            <div className="mt-3 flex flex-row items-center justify-center space-x-4 text-gray-800 md:justify-start dark:text-gray-200">
+            <div className="mt-3 flex flex-row justify-center space-x-4 text-gray-800 md:justify-start dark:text-gray-200">
               <a
                 href="#projects"
-                className="flex items-center rounded-md border bg-blue-600 px-2 py-2 text-white transition-colors duration-300 hover:bg-blue-800 md:px-4 dark:bg-blue-600 dark:hover:bg-blue-400"
+                className="flex items-center rounded-md border bg-blue-600 px-2 py-2 text-white transition-colors duration-300 hover:bg-blue-800 md:px-4 lg:text-lg dark:bg-blue-600 dark:hover:bg-blue-400"
               >
                 {t.home.projectsButton}
-                <ArrowDown className="ml-1 h-4 w-4 md:ml-4" />
+                <ArrowDown className="ml-1 h-4 w-4 lg:ml-4 lg:h-6 lg:w-6" />
               </a>
               <a
                 href="#"
-                className="flex items-center rounded-md border border-gray-400 px-2 py-2 transition-colors duration-300 hover:bg-gray-200 md:px-4 dark:border-gray-600 dark:hover:bg-gray-700"
+                className="flex items-center rounded-md border border-gray-400 px-2 py-2 transition-colors duration-300 hover:bg-gray-200 md:px-4 lg:text-lg dark:border-gray-600 dark:hover:bg-gray-700"
                 onClick={handleDownloadCV}
               >
-                <Download className="mr-1 h-4 w-4 md:mr-4" />
+                <Download className="mr-1 h-4 w-4 lg:mr-4 lg:h-6 lg:w-6" />
                 {t.home.downloadCVButton}
               </a>
             </div>
           </div>
-          <div className="mt-12 w-64 rounded-full border-4 border-blue-950 md:w-80">
-            <div className="overflow-hidden rounded-full md:items-end">
+          <div className="mt-8 md:mt-20 md:flex md:w-1/2 md:justify-end lg:mt-24">
+            <div className="w-64 rounded-full border-4 border-blue-950 md:w-70 lg:w-90">
               <img src={profile} alt="José Tebelán" className="rounded-full" />
             </div>
           </div>
