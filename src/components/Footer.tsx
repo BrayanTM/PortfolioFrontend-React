18124e1e7d7ab234988f304352b1398163018429
BrayanTM <brayantebelan@gmail.com>
2026-02-09 23:04:40 -0600

    feat: agrega traducción para el texto del pie de página en inglés y español

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,13 @@
+import { useLanguage } from "@/context/LanguageContext";
+
 export const Footer = () => {
+  const { t } = useLanguage();
   return (
     <>
       <footer>
         <div className="flex flex-col items-center justify-center bg-slate-200 p-4 text-center text-sm text-gray-600 dark:bg-gray-900 dark:text-gray-400">
           <p>
-            &copy; {new Date().getFullYear()} José Tebelán. {` `}
+            &copy; {new Date().getFullYear()} {t.footer.text}
           </p>
         </div>
       </footer>
